91a8064a53fc5a8d716a7885230304589a20cbd4
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-06-02 17:14:58 +0300

    fixed the server problem and it's starting now

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("Starting...")
 	mux := http.NewServeMux()
-	server := http.Server{}
-	handler := server.Handler
+	rh := http.RedirectHandler("http://localhost", 307)
+
+	mux.Handle("/", rh)
+
+	log.Println("Listening...")
+
+	http.ListenAndServe(":8080", mux)
 }
