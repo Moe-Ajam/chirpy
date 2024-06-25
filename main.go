92a57fbb4af34f01b84b70251f0dd5a59a5bba7e
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-06-25 22:12:26 +0300

    Adding hits count

--- main.go
+++ main.go
@@ -6,28 +6,45 @@ import (
 	"net/http"
 )
 
-func main() {
-	fmt.Println("Starting...")
-	port := "8080"
-	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+type apiConfig struct {
+	fileserverHits int
+}
 
-	server := &http.Server{
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hits:", cfg.fileserverHits)
+	fmt.Fprintln(w, "Hits:", cfg.fileserverHits)
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+}
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	apiCfg := apiConfig{fileserverHits: 0}
+
+	mux := http.NewServeMux()
+	// mux.Handle("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot))))
+	fileServer := http.FileServer(http.Dir(filepathRoot))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fileServer)))
+
+	mux.HandleFunc("/metrics", apiCfg.metricHandler)
+	mux.HandleFunc("/reset", apiCfg.reset)
+
+	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Something went wrong")
-	}
-
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
