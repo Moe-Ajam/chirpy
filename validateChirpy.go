0f71748a46c23341983b9026fd43db3401bb2236
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-06-29 22:30:38 +0300

    Added the chirpy bad words validation

--- validateChirpy.go
+++ validateChirpy.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type responseBody struct {
-	Valid bool `json:"valid"`
+	CleanedBody string `json:"cleaned_body"`
 }
 
 func validateChirpyRequestHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,15 +30,33 @@ func validateChirpyRequestHandler(w http.ResponseWriter, r *http.Request) {
 	if len(params.Body) > 140 {
 		respondWithError(w, 400, "Chirp is too long")
 		return
-	} else {
-		respondWithJSON(w, 200, responseBody{Valid: true})
-		return
 	}
 
+	// respondWithJSON(w, 200, responseBody{Valid: true})
+
+	cleanedBody := validateProfane(params.Body)
+
+	respondWithJSON(w, 200, responseBody{CleanedBody: cleanedBody})
 	// TODO: remove if not needed
 	// do i really need this ??
 	// respondWithJSON(w, 200, params)
 
 	// ..
 	// params is a struct with data populated successfully
+}
+
+func validateProfane(s string) string {
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+
+	words := strings.Split(s, " ")
+
+	for i, word := range words {
+		for _, profaneWord := range profaneWords {
+			if strings.ToLower(word) == strings.ToLower(profaneWord) {
+				words[i] = "****"
+			}
+		}
+	}
+
+	return strings.Join(words, " ")
 }
